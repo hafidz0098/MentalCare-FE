81ab470f4bb2984cf0175e04491722108eaa6b8d
hafidz0098 <hafidznak123@gmail.com>
2024-06-22 10:22:18 +0700

    up

--- pages/topik/index.jsx
+++ pages/topik/index.jsx
@@ -80,12 +80,8 @@ function TopikIndex(props) {
   const { topiks } = props;
   const [isLoading, setIsLoading] = useState(true);
   const [globalFilter, setGlobalFilter] = useState("");
-  const token = Cookies.get("token");
 
   useEffect(() => {
-    if (!token) {
-      Router.push("/login");
-    }
     // Set timeout to simulate loading state
     const timeout = setTimeout(() => {
       setIsLoading(false);
